4736dbf3a898dd788212cafa67a960e8389aef0e
Aleksander Palyan <freeday@apmpc.dp.ua>
2011-09-02 18:44:29 +0300

    little fix

--- lib/redmine_departments/issue_patch.rb
+++ lib/redmine_departments/issue_patch.rb
@@ -19,10 +19,12 @@ module RedmineDepartments
 
     module InstanceMethods
       def assignable_users_with_filter
-        if tracker.is_in_roadmap
+        if tracker.is_in_roadmap && new_record?
           # fast change for clear user list
           # TODO: move role and tracker to config
-          project.members.select {|m| m.roles.detect {|role| role.id == 3}}.collect {|m| m.user}.sort
+          users = project.members.select {|m| m.roles.detect {|role| role.id == 3}}.collect {|m| m.user}.sort
+          users << author if author
+          users.uniq.sort
         else
           assignable_users_without_filter
         end
